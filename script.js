c0b787e2eada87e3c24565c2bf0bde8d3640e0bd
Bruno L Nogueira <nugobruno@gmail.com>
2020-09-16 03:03:42 -0300

    Corrigindo erros das pixels com dois digitos

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const generateBoard = document.querySelector('#generate-board');
 const newDocument = document.querySelector('#new-document');
 const colorPalette = document.querySelector('#color-palette');
 const pixelBoard = document.querySelector('#pixel-board');
+const windowView = document.querySelector('.window-view');
 
 const firstColor = document.querySelector('.color0');
 const secondColor = document.querySelector('.color1');
@@ -18,6 +19,7 @@ generateBoard.addEventListener('click', function() {
     generatePixels(boardSize.value);
     boardSize.value = '';
     pixelBoard.style.display = 'inline-block';
+    windowView.style.display = 'block';
 
     firstColor.addEventListener('click', setColorPalette);
     secondColor.addEventListener('click', setColorPalette);
@@ -44,12 +46,34 @@ function generatePixels(size) {
   pixelBoard.style.heigth = (size * 42) + 'px';
   pixelBoard.style.width = (size * 42) + 'px';
   
-  for (let yIndex = 1; yIndex <= size; yIndex += 1) {
-    for (let xIndex = 1; xIndex <= size; xIndex += 1) {
+  for (let yIndex = 0; yIndex < size; yIndex += 1) {
+    for (let xIndex = 0; xIndex < size; xIndex += 1) {
       const pixel = document.createElement('div');
       pixel.className = 'pixel';
       pixelBoard.appendChild(pixel);
+      const pixelsStyle = document.createElement('style');
+      const setClass = 'px-' + (yIndex + 1).toString() + '-' + (xIndex + 1).toString();
+      pixel.setAttribute('onclick', 'paintPixel(this.className)');
+      pixel.className = 'pixel';
+      pixel.className += ' ' + setClass;
+      pixelBoard.appendChild(pixel);
+      pixel.appendChild(pixelsStyle);
     }
   }
-  
+}
+
+function paintPixel (readClass) {
+  let arrayOfClass = readClass.split('-');
+  const setClass = '.px-' + arrayOfClass[1] + '-' + arrayOfClass[2];
+  const paint = document.querySelector(setClass);
+
+  selectedColor();
+  paint.style.backgroundColor = selectedColor();
+}
+
+function selectedColor() {
+  let whatColorSelected = document.querySelector('.selected').classList[1];
+  let elem = document.querySelector('.' + whatColorSelected);
+  let color = getComputedStyle(elem).backgroundColor;
+  return color;
 }
